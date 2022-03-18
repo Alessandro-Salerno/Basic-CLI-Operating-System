a4dd5b8dc8928404530123f822f7f12d8dd65128
tzyvoski <67583725+tzyvoski@users.noreply.github.com>
2022-03-18 18:15:43 +0100

    Simplified main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -30,17 +30,18 @@ For more information, please refer to <https://unlicense.org>
 #include "shell.h"
 #include "keyboard.h"
 #include "ostime.h"
+#include "easy.h"
 
 void kernel_main()
 {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    textcolor(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
 
-    print_str("WARN: This software comes with no warranty or support.\n\n");
-    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
+    printf("WARN: This software comes with no warranty or support.\n\n");
+    textcolor(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
 
-    print_str("This software is in the Public Domain and is distributed under the \"Unlicese\".\n");
-    print_str("Basic Kernel v0.0.4!\n\n");
+    printf("This software is in the Public Domain and is distributed under the \"Unlicense\".\n");
+    printf("Basic Kernel v0.0.4!\n\n");
 
     shell_main();
 }
