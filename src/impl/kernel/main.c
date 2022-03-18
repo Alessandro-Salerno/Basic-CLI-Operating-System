9b0051dd0d627dc7c851e83ceb6609a2e82ce7e4
tzyvoski <67583725+tzyvoski@users.noreply.github.com>
2022-03-18 16:57:27 +0100

    Updated boot screen

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -9,11 +9,11 @@ void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     
-    print_str("WARN: This software is in Alpha and comes with no warranty or support.\n\n");
+    print_str("WARN: This software comes with no warranty or support.\n\n");
     print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
     
-    print_str("Copyright Alessandro Salerno 2021. All Rights Reserved.\n");
-    print_str("SalernOS Kernel v0.0.3!\n\n");
+    print_str("This software is in the Public Domain and is distributed under the \"Unlicese\".\n");
+    print_str("Basic Kernel v0.0.4!\n\n");
 
     shell_main();
 }
